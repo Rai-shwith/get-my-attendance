dfc2c930ee2798923d3949820941ae2ea272e462
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-12-10 20:50:08 +0530

    chore: initial design for web interface attendace system

---
+++ public/static/js/master.js
@@ -0,0 +1,47 @@
+function showRegistrationPhase() {
+    document.getElementById('registrationPhase').classList.remove('hidden');
+}
+
+function submitRegistrationTime() {
+    const linkContainer = document.getElementById('linkContainer');
+    linkContainer.classList.remove('hidden');
+}
+
+function copyToClipboard() {
+    const link = document.getElementById('generatedLink').textContent;
+    navigator.clipboard.writeText(link).then(() => {
+        alert('Link copied to clipboard!');
+    });
+}
+
+function addLogEntry() {
+    const logView = document.getElementById('logView');
+    
+    // Simulated log messages
+    const logMessages = [
+        { text: 'Attendance successfully recorded for <b>Jane Smith [2MS23EE123]</b>.', type: 'log-success' },
+        { text: 'Jane Smith [2MS23EE123] has already marked attendance.', type: 'log-info' },
+        { text: 'An unregistered user attempted to mark attendance.', type: 'log-warning' },
+        { text: 'Jane Smith’s information has been erased from the system.', type: 'log-error' }
+    ];
+
+}
+
+
+
+// Function to close all other sections and open only given section
+function showSection(id){
+    // Hide the cover
+    if (id == 'homePage'){
+        document.getElementById('backIcon').classList.add('hidden');
+    } else{
+        document.getElementById('backIcon').classList.remove('hidden');
+    }
+    console.log("Showing only section: " + id);
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    document.getElementById(id).classList.remove('hidden');
+}
