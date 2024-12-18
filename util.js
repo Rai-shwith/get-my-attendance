773157f2de3b2af4d3286432abe1384749d3c411
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-12-18 20:47:55 +0530

    organize: removed old files

--- util.js
+++
@@ -1,8 +0,0 @@
-   // Function to close the server
-   const closeServer = async (server) => {
-    server.close(() => {
-        log(yellow, 'Server stopped gracefully.');
-        process.exit(0);
-    });
-}
-
