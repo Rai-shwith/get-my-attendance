282e91b8920b5d1701ad4de840352280a4d1bca8
Ashwith Rai <147788165+Rai-shwith@users.noreply.github.com>
2024-12-22 01:22:29 +0530

    style:dark mode for homepage

--- public/static/js/helper.js
+++ public/static/js/helper.js
@@ -1,10 +1,4 @@
-const toggleButton = document.getElementById("theme-toggle");
-const body = document.body;
-
-toggleButton.addEventListener("click", () => {
-    body.classList.toggle("dark-theme");
-    const elements = document.querySelectorAll("header, .card, .button, footer");
-    elements.forEach(element => {
-        element.classList.toggle("dark-theme");
-    });
+const themeCheckbox = document.getElementById("theme");
+themeCheckbox.addEventListener("change", () => {
+    document.body.classList.toggle('dark-theme');
 });
